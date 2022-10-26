65d8e5d89aef0a5e2264e32186b37799a78c971d
Anastasia Andreeva <nastiy1890@yandex.ru>
2022-10-26 14:12:53 -0700

    feature: added new features

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -8,13 +8,15 @@ function SearchForm(props) {
   return (
     <form className="searchform">
       <div className="searchform__container">
-        <img src={searchIcon} alt="Search Icon" />
+        <img className="searchform__icon" src={searchIcon} alt="Search Icon" />
         <input type="text" className="searchform__input" placeholder="Movie" />
       </div>
       <button className="searchform__button" type="submit"></button>
       <div className="searchform__line"></div>
-      <button className="searchform__toggle-button"></button>
-      <p className="searchform__toggle-button-name">Short films</p>
+      <div className="searchform__button-container">
+        <button className="searchform__toggle-button"></button>
+        <p className="searchform__toggle-button-name">Short films</p>
+      </div>
     </form>
   );
 }
